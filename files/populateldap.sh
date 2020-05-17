7185fcf1928c9a9d44ad84aadf82ec26491686e7
Stephen Ancliffe <stephen.ancliffe@gmail.com>
2020-05-17 14:05:05 -0400

    Backing up changes WIP

--- files/populateldap.sh
+++ files/populateldap.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-ldapmodify -Q -Y EXTERNAL -H ldapi:/// -f logging.ldif
-ldapadd -x -D cn=admin,dc=home,dc=com -W -f add_content.ldif
+ldapmodify -Q -Y EXTERNAL -H ldapi:/// -f ../ldif/logging.ldif
+ldapadd -x -D cn=admin,dc=home,dc=com -W -f ../ldif/add_content.ldif
 ldapsearch -x -LLL -H ldap:/// -b dc=home,dc=com
 slapcat -v -b dc=home,dc=com -l out.ldif
