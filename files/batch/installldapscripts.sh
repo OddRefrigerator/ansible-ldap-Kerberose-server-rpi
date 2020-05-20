246b7a23a683bc8c4220c585bc33983bbc903fb0
Stephen Ancliffe <stephen.ancliffe@gmail.com>
2020-05-19 23:14:38 -0400

    backing up work

--- files/batch/installldapscripts.sh
+++ files/batch/installldapscripts.sh
@@ -2,3 +2,4 @@
 apt install -y ldapscripts 
 sh -c "echo -n 'password123' > /etc/ldapscripts/ldapscripts.passwd"
 chmod 400 /etc/ldapscripts/ldapscripts.passwd
+#sudo cp ldapscripts.conf /etc/ldapscripts/ldapscripts.conf
--- files/batch/populateldap.sh
+++ files/batch/populateldap.sh
@@ -8,28 +8,24 @@ sudo gunzip /etc/ldap/schema/kerberos.schema.gz
 sudo ldap-schema-manager -i /etc/ldap/schema/kerberos.schema
 
 #Create krb5 container
-sudo kdb5_ldap_util -D cn=admin,dc=home,dc=com -w password123 -H ldap://auth.home.com create -subtrees dc=home,dc=com -P password123 -r HOME.COM -s 
-
-#Add content
-sudo ldapadd -Q -Y EXTERNAL -H ldapi:/// -f add_content.ldif -c
-#sudo ldapadd -x -D cn=admin,dc=home,dc=com -w password123 -H ldap://auth.home.com -f add_content.ldif -c
+sudo kdb5_ldap_util -D cn=admin,dc=home,dc=com -w password123 -H ldap://auth.home.com create -subtrees dc=home,dc=com -P password123 -r HOME.COM -s
 
 #apply ldif for krb5
 sudo ldapmodify -Q -Y EXTERNAL -H ldapi:/// -f krb5Index.ldif -c
-sudo ldapadd -Q -Y EXTERNAL -H ldapi:/// -f krb5admin.ldif -c
 sudo ldapadd -Q -Y EXTERNAL -H ldapi:/// -f krb5perms.ldif -c
+sudo ldapadd -Q -Y EXTERNAL -H ldapi:/// -f krb5admin.ldif -c
+#sudo ldapadd -x -D cn=admin,dc=home,dc=com -w password123 -H ldap://auth.home.com -f krb5admin.ldif -c
+sudo ldapsearch -Y EXTERNAL -H ldapi:/// -b uid=kdc-service,dc=home,dc=com
+
+#Set password and stash in service.kefile
+sudo ldappasswd -x -D cn=admin,dc=home,dc=com -w password123 -s password123 uid=kdc-service,dc=home,dc=com
+sudo ldappasswd -x -D cn=admin,dc=home,dc=com -w password123 -s password123 uid=kadmin-service,dc=home,dc=com
+sudo kdb5_ldap_util stashsrvpw -f /etc/krb5kdc/ldapservice.keyfile uid=kdc-service,dc=home,dc=com
+sudo kdb5_ldap_util stashsrvpw -f /etc/krb5kdc/ldapservice.keyfile uid=kadmin-service,dc=home,dc=com
+
+#Add content
+#sudo ldapadd -Q -Y EXTERNAL -H ldapi:/// -f add_content.ldif -c
 
 sudo ldapsearch -Y EXTERNAL -H ldapi:/// -b dc=home,dc=com
-slapcat -v -b dc=home,dc=com -l out.ldif
+slapcat -v -b dc=home,dc=com -l backup.ldif
 
-#sudo kdb5_ldap_util -D uid=admin,dc=auth,dc=home,dc=com -w password123 stashsrvpw uid=kdc-service,dc=auth,dc=home,dc=com
-
-#dn: cn={4}kerberos,cn=schema,cn=config
-# cn=kerberos,cn=schema,cn=config
-# cn=krbContainer,dc=home,dc=com
-#sudo ldapadd -x -D cn=admin,dc=home,dc=com -w password123 -H ldap://auth.home.com -f krb5admin.ldif -c
-#sudo ldapadd -x -D cn=admin,dc=home,dc=com -w password123 -H ldap://auth.home.com -f krb5perms.ldif -c
-#sudo ldapsearch -Y EXTERNAL -H ldapi:/// -b cn=kerberos,cn=schema,cn=config
-#sudo ldapsearch -Y EXTERNAL -H ldapi:/// -b dc=auth,dc=home,dc=com
-#sudo kdb5_ldap_util -D uid=admin,dc=auth,dc=home,dc=com -w password123 stashsrvpw uid=kdc-service,dc=auth,dc=home,dc=com
-#sudo kdb5_ldap_util -D cn=admin,dc=home,dc=com -w password123 -H ldap://auth.home.com create -subtrees dc=home,dc=com -P password123 -r HOME.COM -s
