8e1f970e8af9a8a84c5ab59c4ad8e9df5c66b39c
Stephen Ancliffe <stephen.ancliffe@gmail.com>
2020-06-03 15:06:55 -0400

    Added KRB5 setup bash script

--- files/batch/populateldap.sh
+++ files/batch/populateldap.sh
@@ -10,22 +10,23 @@ sudo ldap-schema-manager -i /etc/ldap/schema/kerberos.schema
 
 #Set password for admin
 #CHANGE THIS--> Password should be set dynamicaly from ENV or prog var
-slapAdmin=password123
+#slapAdmin=password123
 pass=$(sudo slappasswd -s $slapAdmin -h {SSHA})
-echo 'olcRootPW: $pass' >> ../ldif/newPassword.ldif
+echo 'olcRootPW: '$pass >> ../ldif/newPassword.ldif
 sudo ldapmodify -Q -Y EXTERNAL -H ldapi:/// -f ../ldif/newPassword.ldif
 
 #Apply ldif for krb5
 sudo ldapmodify -Q -Y EXTERNAL -H ldapi:/// -f ../ldif/krb5Index.ldif -c
 sudo ldapadd -Q -Y EXTERNAL -H ldapi:/// -f ../ldif/krb5perms.ldif -c
 #CHANGE THIS--> Password should be set dynamicaly from ENV or prog var
-sudo ldapadd -x -D cn=admin,dc=home,dc=com -w password123 -H ldap://auth.home.com -f ../ldif/krb5admin.ldif -c
+sudo ldapadd -x -D cn=admin,dc=home,dc=com -w $slapAdmin -H ldap://auth.home.com -f ../ldif/krb5admin.ldif -c
 
 #Set password and stash in service.kefile
 #CHANGE THIS--> Password should be set dynamicaly from ENV or prog var
-sudo ldappasswd -x -D cn=admin,dc=home,dc=com -w password123 -s password123 uid=kdc-service,dc=home,dc=com
+sudo ldappasswd -x -D cn=admin,dc=home,dc=com -w $slapAdmin -s $ldapPassword uid=kdc-service,dc=home,dc=com
 #CHANGE THIS--> Password should be set dynamicaly from ENV or prog var
-sudo ldappasswd -x -D cn=admin,dc=home,dc=com -w password123 -s password123 uid=kadmin-service,dc=home,dc=com
+sudo ldappasswd -x -D cn=admin,dc=home,dc=com -w $slapAdmin -s $ldapPassword uid=kadmin-service,dc=home,dc=com
+#CHANGE THIS--> no option to script password input
 sudo kdb5_ldap_util stashsrvpw -f /etc/krb5kdc/ldapservice.keyfile uid=kdc-service,dc=home,dc=com
 sudo kdb5_ldap_util stashsrvpw -f /etc/krb5kdc/ldapservice.keyfile uid=kadmin-service,dc=home,dc=com
 sudo systemctl restart slapd
@@ -33,5 +34,7 @@ sudo systemctl restart slapd
 #Create krb5 container
 sudo sed -i 's/#database_module = openldap_ldapconf/database_module = openldap_ldapconf/g' /etc/krb5.conf
 #CHANGE THIS--> Password should be set dynamicaly from ENV or prog var
-sudo kdb5_ldap_util -D cn=admin,dc=home,dc=com -w password123 -H ldap://auth.home.com create -subtrees dc=home,dc=com -P password123 -r HOME.COM -s
+sudo kdb5_ldap_util -D cn=admin,dc=home,dc=com -w $slapAdmin -H ldap://auth.home.com create -subtrees cn=krbContainer,dc=home,dc=com -P $ldapPassword -r HOME.COM -s
+
+
 
