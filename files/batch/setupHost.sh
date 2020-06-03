c7e18d4d217e5f236129c4835db07b2dc8a3b9cf
Stephen Ancliffe <stephen.ancliffe@gmail.com>
2020-06-03 15:11:14 -0400

    Add KRB5 setup script. Modified scripts

--- files/batch/setupHost.sh
+++ files/batch/setupHost.sh
@@ -3,3 +3,6 @@ sudo hostnamectl set-hostname auth.home.com
 sudo timedatectl set-timezone America/Toronto
 sudo apt update 
 sudo apt upgrade -y
+export ldapPassword="password123"
+export slapAdmin="password123"
+export realm="HOME.COM"
