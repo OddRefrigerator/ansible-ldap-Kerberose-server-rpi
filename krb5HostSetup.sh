4bd9dae8f0ec7c988a7b16f206f4889167eeea0c
Stephen Ancliffe <stephen.ancliffe@gmail.com>
2020-06-22 14:10:59 -0400

    Template Playbook changes

--- krb5HostSetup.sh
+++ krb5HostSetup.sh
@@ -8,10 +8,16 @@ kadmin.local -q "ktadd -k /etc/krb5.keytab host/auth.home.com@HOME.COM"
 kadmin.local -q "addprinc -randkey host/192.168.1.10@HOME.COM"
 kadmin.local -q "ktadd -k /etc/krb5.keytab host/192.168.1.10@HOME.COM"
 
-sudo kadmin.local -q "addprinc -pw toor pi"
-sudo kadmin.local -q "addprinc -pw TheFlyingFish pi"
-ldapadd -x -D cn=admin,dc=home,dc=com -w password123 -H ldap://auth.home.com -f add_content.ldif -c
+sudo kadmin.local -q "addprinc -randky krb5-kdc"
+sudo kadmin.local -q "addprinc -randky krb5-admin"
 
+sudo kadmin.local -q "addprinc -pw toor pi"
+sudo kadmin.local -q "addprinc -pw mediaPlayer mediaPlayer"
+
+ldapadd -x -D cn=admin,dc=home,dc=com -w password123 -H ldap://auth.home.com -f add_content.ldif -c
+ldapsearch -Q -LLL -Y EXTERNAL -H ldapi:/// -b dc=home,dc=com
+
+#check a_dn_pat: gidNumber=0+uidNumber=0,cn=peercred,cn=external,cn=auth
 #ldapadd -x -D cn=admin,dc=home,dc=com -w password123 -H ldap://auth.home.com -f changeAdminPW.ldif
 #kdb5_ldap_util stashsrvpw -f /etc/krb5kdc/ldapservice.keyfile cn=krb5-admin,dc=home,dc=com
 #kdb5_ldap_util stashsrvpw -f /etc/krb5kdc/ldapservice.keyfile cn=krb5-kdc,dc=home,dc=com
